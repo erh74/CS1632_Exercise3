a380f3be42d0767351138d089cb5efa61b3f0138
erh74 <erh74@pitt.edu>
2017-10-07 06:38:34 -0400

    Add files via upload

---
+++ RentACatTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import org.mockito.*;
+
+
+public class RentACatTest {
+
+  @Test
+  public void testCatExistsExists(){
+    //make an ArrayList of doubled cats
+    // ArrayList<Cat> arrListOfMockCats = new ArrayList<Cat>();
+    // arrListOfMockCats.add(mock(Double Cat(1,"testCat1").class));
+    // arrListOfMockCats.add(mock(Double Cat(2,"testCat2").class));
+    // arrListOfMockCats.add(mock(Double Cat(3,"testCat3").class));
+    Cat c = Mockito.mock(Cat.class);
+
+
+    //call "if cat exists" function
+  }
+
+}
